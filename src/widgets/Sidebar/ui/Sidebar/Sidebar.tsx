ffcc188e9728299010f8abb9589235543b96bd17
Yarex Marvin <yarexmarvin@yahoo.com>
2023-11-21 10:09:25 +0400

    added sidebar

---
+++ src/widgets/Sidebar/ui/Sidebar/Sidebar.tsx
@@ -0,0 +1,31 @@
+import { classNames } from 'shared/lib/classNames';
+import cls from './Sidebar.module.scss';
+
+import { useState, type PropsWithChildren } from 'react';
+import { ThemeSwitcher } from 'shared/ui/ThemeSwitcher';
+
+interface SidebarProps {
+  className?: string;
+}
+
+export function Sidebar(props: PropsWithChildren<SidebarProps>) {
+  const { className } = props;
+
+  const [collapsed, setCollapsed] = useState(false)
+
+  const toggleCollapsed = () => setCollapsed(prev => !prev);
+  console.log(collapsed)
+  console.log(classNames({ cls: cls.Sidebar, mods: { [cls.collapsed]: collapsed }, additional: [className] }))
+
+  return (
+    <div
+      className={classNames({ cls: cls.Sidebar, mods: { [cls.collapsed]: collapsed }, additional: [className] })}>
+
+      <button onClick={toggleCollapsed}>toggle</button>
+      <div className={cls.switchers}>
+        <ThemeSwitcher />
+      </div>
+    </div>
+
+  );
+}
