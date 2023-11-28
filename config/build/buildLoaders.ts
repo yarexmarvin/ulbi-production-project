551a618fa65eec5cab3377b5eecf4ceb91b9bb1f
Yarex Marvin <yarexmarvin@yahoo.com>
2023-11-28 21:23:32 +0400

    added babel and extract plugin

--- config/build/buildLoaders.ts
+++ config/build/buildLoaders.ts
@@ -44,13 +44,34 @@ export const buildLoaders = ({ isDev }: BuildOptions): webpack.RuleSetRule[] => 
     ],
   }
 
+  const babelLoader = {
+    test: /\.(js|jsx|ts|tsx)$/,
+    exclude: /node_modules/,
+    use: {
+      loader: "babel-loader",
+      options: {
+        presets: ['@babel/preset-env'],
+        "plugins": [
+          ["i18next-extract",
+            {
+              locales: ['ru', 'en'],
+              "keyAsDefaultValue": true
+            }
+          ]
+        ]
+      }
+    }
+  }
+
 
 
 
   return [
+    // babelLoader до tsLoader
+    babelLoader,
     tsLoader,
     sassLoader,
     svgLoader,
-    fileLoader
+    fileLoader,
   ]
 }
